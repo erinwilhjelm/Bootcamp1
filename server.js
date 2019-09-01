71185d4c9b24c7355e93434537817e0691ef456a
erinwilhjelm <erin.wilhjelm@ufl.edu>
2019-09-01 16:43:12 -0400

    final version

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ let listingData = JSON.parse(rawdata);
 response.end(JSON.stringify(listingData));
 
 } else {
-    response.writeHead(404, {"Content-Type": "text/plain"});
-  	response.write('Bad gateway');
+    response.writeHead( 404);
+  	response.write('Bad gateway error');
   	response.end();
 }
 };
@@ -31,10 +31,8 @@ var server = http.createServer(requestHandler);
 // the server is now started, listening for requests on port 8080 - go to your browerd and paste in http://127.0.0.1:8080
 server.listen(port, function() {
 //once the server is listening, this callback function is executed
-console.log('Server listening on: http://localhost:' + port + '/listings');
+console.log('Server listening on: http://localhost:' + port );
 });
-console.log('Is the server started?');
-
 
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
