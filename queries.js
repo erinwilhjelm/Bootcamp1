4af77b5335579713f5ec3065ec6d20ecfb640b46
Erin Wilhjelm <erin.wilhjelm@ufl.edu>
2019-09-17 19:39:55 -0400

    Update queries.js

--- queries.js
+++ queries.js
@@ -18,20 +18,19 @@ var findLibraryWest = function() {
     then log it to the console. 
    */
   Listing.findOne({name:"Library West"}, function(err, listingData) {
+    
     if (err) throw err;
+    
     console.log("Found : ", listingData.name, "\n", listingData);
   });
 
 };
 var removeCable = function() {
-  /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
-    on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
-   */
 
   Listing.findOne({code : "CABL"}, function(err, listingData) {
+    
       if (err) throw err;
+    
       Listing.deleteOne(function(err) {
         if (err) throw err;
       })
@@ -39,13 +38,7 @@ var removeCable = function() {
   });
 };
 var updatePhelpsLab = function() {
-  /*
-    Phelps Lab address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
-    
-    Correct Address: 1953 Museum Rd, Gainesville, FL 32603
 
-   */
   Listing.findOne({code:"PHL"}, function(err, listingData){
     if (err) throw err;
     listingData.address = "1953 Museum Rd, Gainesville, FL 32603";
@@ -57,11 +50,11 @@ var updatePhelpsLab = function() {
 
 };
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
-   */
+
   Listing.find(function(err, listingData) {
+    
     if (err) throw err;
+    
     console.log("\n All Listings \n", listingData);
   });
 };
